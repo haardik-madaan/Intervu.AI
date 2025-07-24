f7edb69c45f16851a449ed195bfd482698216540
Haardik Madaan <madaanhaardik@gmail.com>
2025-07-25 01:02:41 +0530

    submit button correction

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -102,7 +102,7 @@ const AuthForm = ({type} : {type : FormType}) => {
 
         <div className="flex flex-row gap-2 justify-center">
           <Image src = "/logo.svg" alt="logo" height={32} width={38}/>
-          <h2 className="text-primary-100">PrepWise</h2> 
+          <h2 className="text-primary-100">Intervu.AI</h2> 
         </div>
 
         <h3>Practice mock interviews with AI</h3>
